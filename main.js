b25ab6bd413a7566584aba355bb9fc35935aced2
Vyomkesh10141928 <74350952+Vyomkesh10141928@users.noreply.github.com>
2020-12-23 11:09:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,7 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+if (width < 1200)
+{
+  hide("mybutton");
+}
